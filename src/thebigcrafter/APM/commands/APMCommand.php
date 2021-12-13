89cc1898f69a0c832b5607738d43dd2eede200b1
MintoD <tobythedev7@outlook.com>
2021-12-13 09:11:48 +0700

    Add list plugins command
    Signed-off-by: MintoD <tobythedev7@outlook.com>

--- src/thebigcrafter/APM/commands/APMCommand.php
+++ src/thebigcrafter/APM/commands/APMCommand.php
@@ -5,37 +5,20 @@ declare(strict_types=1);
 namespace thebigcrafter\APM\commands;
 
 use CortexPE\Commando\BaseCommand;
-use thebigcrafter\APM\commands\subcommands\AddRepoCommand;
-use thebigcrafter\APM\commands\subcommands\ListRepoCommand;
-use thebigcrafter\APM\commands\subcommands\RemoveRepoCommand;
-use thebigcrafter\APM\commands\subcommands\UpdateCommand;
-use thebigcrafter\APM\commands\subcommands\InstallPluginCommand;
-use thebigcrafter\APM\forms\MenuForm;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use thebigcrafter\APM\APM;
+use thebigcrafter\APM\commands\subcommands\AddRepoCommand;
+use thebigcrafter\APM\commands\subcommands\InstallPluginCommand;
+use thebigcrafter\APM\commands\subcommands\ListPluginCommand;
+use thebigcrafter\APM\commands\subcommands\ListRepoCommand;
 use thebigcrafter\APM\commands\subcommands\RemovePluginCommand;
+use thebigcrafter\APM\commands\subcommands\RemoveRepoCommand;
+use thebigcrafter\APM\commands\subcommands\UpdateCommand;
+use thebigcrafter\APM\forms\MenuForm;
 
 class APMCommand extends BaseCommand
 {
-    protected function prepare(): void
-    {
-        $subCommands = [
-            new AddRepoCommand("add-repo", APM::getLanguage()->translateString("add.repo.command.description")),
-            new RemoveRepoCommand("remove-repo", APM::getLanguage()->translateString("remove.repo.command.description")),
-            new ListRepoCommand("list-repo", APM::getLanguage()->translateString("list.repo.command.description")),
-            new UpdateCommand("update", APM::getLanguage()->translateString("update.command.description")),
-            new InstallPluginCommand("install", APM::getLanguage()->translateString("install.plugin.form.title")),
-            new RemovePluginCommand("remove", APM::getLanguage()->translateString("remove.command.description"))
-        ];
-        $this->setDescription(APM::getLanguage()->translateString("apm.command.description"));
-        $this->setPermission("apm.cmd");
-
-        foreach ($subCommands as $subCommand) {
-            $this->registerSubCommand($subCommand);
-        }
-    }
-
     /**
      * @param CommandSender $sender
      * @param string $aliasUsed
@@ -49,6 +32,25 @@ class APMCommand extends BaseCommand
             $sender->sendForm(MenuForm::getMenuForm());
         } else {
             $this->sendUsage();
+        }
+    }
+
+    protected function prepare(): void
+    {
+        $subCommands = [
+            new AddRepoCommand("add-repo", APM::getLanguage()->translateString("add.repo.command.description")),
+            new RemoveRepoCommand("remove-repo", APM::getLanguage()->translateString("remove.repo.command.description")),
+            new ListRepoCommand("list-repo", APM::getLanguage()->translateString("list.repo.command.description")),
+            new UpdateCommand("update", APM::getLanguage()->translateString("update.command.description")),
+            new InstallPluginCommand("install", APM::getLanguage()->translateString("install.plugin.form.title")),
+            new RemovePluginCommand("remove", APM::getLanguage()->translateString("remove.command.description")),
+            new ListPluginCommand("list", APM::getLanguage()->translateString("list.command.description")),
+        ];
+        $this->setDescription(APM::getLanguage()->translateString("apm.command.description"));
+        $this->setPermission("apm.cmd");
+
+        foreach ($subCommands as $subCommand) {
+            $this->registerSubCommand($subCommand);
         }
     }
 }
---
+++ src/thebigcrafter/APM/commands/subcommands/ListPluginCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace thebigcrafter\APM\commands\subcommands;
+
+use CortexPE\Commando\BaseSubCommand;
+use pocketmine\command\CommandSender;
+use thebigcrafter\APM\APM;
+
+class ListPluginCommand extends BaseSubCommand
+{
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
+    {
+        foreach (APM::$reposPluginsCache as $plugin) {
+            $sender->sendMessage($plugin["name"]);
+        }
+    }
+
+    protected function prepare(): void
+    {
+    }
+}
