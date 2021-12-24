e6d263cf36f7a144e493fc04a885d9c9bdd53333
MintoD <tobythedev7@outlook.com>
2021-12-24 14:24:14 +0700

    Fix PHPStan
    Signed-off-by: MintoD <tobythedev7@outlook.com>

--- src/commands/subcommands/UpgradePluginCommand.php
+++ src/commands/subcommands/UpgradePluginCommand.php
@@ -21,6 +21,13 @@ class UpgradePluginCommand extends BaseSubCommand {
         $this->registerArgument(0, new RawStringArgument("plugin name"));
     }
 
+    /**
+     * @param CommandSender $sender
+     * @param string $aliasUsed
+     * @param array<string> $args
+     *
+     * @return void
+     */
     public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
     {
         if($sender instanceof Player) {
